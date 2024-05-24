15bacb39fad0ee89430625f8fda61138715f657f
Jincheng Sun <sjc951213@gmail.com>
2024-05-24 09:27:37 -0400

    add pdf_to_text_chunk method

--- parser/fitz.py
+++ parser/fitz.py
@@ -1,57 +1,24 @@
-from pathlib import Path
-from typing import List, Union
-
 import pymupdf
 from langchain.text_splitter import CharacterTextSplitter
-from loguru import logger
 
-max_size = 1024
-chunk_size = 1024
-chunk_overlap = 128
 
-all_chunks = []
-splitter = CharacterTextSplitter(
-    separator="\n",
-    keep_separator=True,
-    chunk_size=1024,
-    chunk_overlap=128,
-)
-
-file_path = "../data/ev_outlook_2023.pdf"
-doc = pymupdf.open(file_path)
-current_text = ""
-for page in doc:
-    text = page.get_text("block")
-
-    if len(text) > max_size:
-        all_chunks.append(
-            {"text": current_text, "metadata": {"page": page.number}}
-        )
+def pdf_to_text_chunk(file_path: str, chunk_size: int = 1000, chunk_overlap: int = 200):
+    chunks = []
+    splitter = CharacterTextSplitter(
+        separator="\n",
+        keep_separator=False,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+    )
+    doc = pymupdf.open(file_path)
+    for page in doc:
+        text = page.get_text("block")
         chunks = splitter.split_text(text)
         for chunk in chunks:
-            logger.info(
-                f"Flushing chunk. Length: {len(chunk)}, page: {page.number}"
-            )
-            all_chunks.append(
-                {"text": chunk, "metadata": {"page": page.number}}
-            )
-        current_text = ""
+            chunks.append({"text": chunk, "metadata": {"page": page.number}})
+    return chunks
 
-    elif len(current_text + text) >= max_size:
-        if current_text != "":
-            all_chunks.append(
-                {"text": current_text, "metadata": {"page": page.number}}
-            )
-        logger.info(
-            f"Flushing chunk. Length: {len(current_text)}, page: {page.number}"
-        )
-        current_text = text
 
-    # Otherwise, add element's text to current chunk, without re-assigning the page number
-    else:
-        current_text += text
-
-# Filter out empty docs
-all_chunks = [
-    chunk for chunk in all_chunks if chunk["text"].strip().replace(" ", "")
-]
+if __name__ == "__main__":
+    _file_path = "../data/ev_outlook_2023.pdf"
+    _chunks = pdf_to_text_chunk(_file_path)
